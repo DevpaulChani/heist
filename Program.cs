54751750c7e6c117fe5eab301bc88b14391a6623
DevpaulChani <devpaulc@gmail.com>
2026-05-11 09:51:59 -0500

    promts

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string input;
+Console.WriteLine("Plan Your Heist!");
+Console.WriteLine("enter a team member's name and save that name");
+Console.WriteLine("enter a team member's skill level and save that skill level with the name");
+Console.WriteLine("enter a team member's courage factor and save that courage factor with the name");
